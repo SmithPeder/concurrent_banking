6783abdfcbedcbfc091c8d82292ae2ffca521903
Peder Smith <smith.peder@gmail.com>
2019-10-18 14:54:30 +0200

    Add Array sum

--- tasks/task1.scala
+++ tasks/task1.scala
@@ -1,9 +1,34 @@
 object Task1{
-    def main(args: Array[String]) {
-        var array = Array.empty[Int]
-        for (i <- 1 to 50) {
-            array :+= i
-        }
-        array.foreach(print)
+  def main(args: Array[String]) {
+    val a = createArray(50)
+    a.foreach(print)
+
+    println("\n" + sumArrayIterative(a))
+
+    println(sumArrayRecursive(a.toList))
+  }
+
+
+  def createArray(value: Int): Array[Int] = {
+    var array = Array.empty[Int]
+    for (i <- 1 to value) {
+        array :+= i
     }
+    return array
+  }
+
+
+  def sumArrayIterative(list: Array[Int]): Int = {
+    var sum = 0
+    for (e <- list) {
+      sum += e
+    }
+    return sum
+  }
+
+
+  def sumArrayRecursive(list: List[Int]): Int = list match {
+      case head :: tail => head + sumArrayRecursive(tail)
+      case Nil => 0
+  }
 }
