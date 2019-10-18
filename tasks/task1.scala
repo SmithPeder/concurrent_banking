291e70a5c93880397a8988f219f1265419b4c6ab
Peder Smith <smith.peder@gmail.com>
2019-10-18 15:17:55 +0200

    Finish Task 1

--- tasks/task1.scala
+++ tasks/task1.scala
@@ -1,14 +1,20 @@
 object Task1{
   def main(args: Array[String]) {
+
     val a = createArray(50)
+
     a.foreach(print)
 
     println("\n" + sumArrayIterative(a))
-
-    println(sumArrayRecursive(a.toList))
+    println("\n" + sumListRecursive(a.toList))
+    println(fib(9))
   }
 
 
+  /*
+   * Funciton createArray
+   * Creates an array of length value
+   */
   def createArray(value: Int): Array[Int] = {
     var array = Array.empty[Int]
     for (i <- 1 to value) {
@@ -18,6 +24,10 @@ object Task1{
   }
 
 
+  /*
+   * Funciton sumArrayIterative
+   * Returns the sum of an array
+   */
   def sumArrayIterative(list: Array[Int]): Int = {
     var sum = 0
     for (e <- list) {
@@ -27,8 +37,27 @@ object Task1{
   }
 
 
-  def sumArrayRecursive(list: List[Int]): Int = list match {
-      case head :: tail => head + sumArrayRecursive(tail)
-      case Nil => 0
+  /*
+   * Funciton sumListRecursive
+   * Returns the sum of a list using pattern mathcing
+   */
+  def sumListRecursive(list: List[Int]): Int = list match {
+    case head :: tail => head + sumListRecursive(tail)
+    case Nil => 0
+  }
+
+  /*
+   * Funciton fib using BigInt
+   * BigInt uses 8 byte == 2^64 while Int uses 4 bytes
+   * which gives it a range of 2^32, giving it 2.1 billion
+   */
+  def fib(value: BigInt): BigInt = {
+    if (value < 2) {
+      value
+    }
+    else {
+      return fib(value-1) + fib(value-2)
+    }
+
   }
 }
